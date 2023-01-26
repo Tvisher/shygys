b742c0950b69151c3da192c5985dc43b05fbd92f
Tvisher <tvisher@mail.ru>
2023-01-26 17:25:48 +0600

    good

---
+++ src/js/app.js
@@ -0,0 +1,73 @@
+'use strict';
+import * as baseFunction from './modules/functions.js';
+import './vendors/vendors.js';
+import AOS from 'aos';
+
+// Проверка поддержки webP
+baseFunction.testWebP();
+
+window.addEventListener('load', (e) => {
+    document.body.style.opacity = 1;
+});
+
+
+
+// Инит и опции библиотеки анимаций
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 25, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 1200, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+//логика работы меню бургер
+document.body.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.closest('[data-burger-menu]')) {
+        target.closest('[data-burger-menu]').classList.toggle('active');
+        document.querySelector('[data-header-menu]').classList.toggle('active');
+        document.body.classList.toggle('hidden');
+    }
+    if (target.closest('[data-close-modal]')) {
+        const closeBtn = target.closest('[data-close-modal]');
+        closeBtn.closest('.show').classList.remove('show');
+    }
+
+
+    if (target.closest('.modal.show') && !target.closest('.modal__content')) {
+        document.querySelector('.modal.show').classList.remove('show');
+    }
+
+
+    if (target.closest('[data-open-modal]')) {
+        const modalBtn = target.closest('[data-open-modal]');
+        const modalId = modalBtn.dataset.openModal;
+        document.querySelector(`[data-modal="${modalId}"]`).classList.add('show');
+
+    }
+
+
+});
+
+// Маска на номера телефона
+document.querySelectorAll('input[type="tel"]').forEach(input => {
+    const mask = IMask(input, {
+        mask: '+{7}(000) 000-00-00'
+    });
+});
+
+
+
